948584a075329722c6ebd270cd6eafcd340c9434
Karl Mohler <karl@SimpleLTC.local>
2018-06-27 09:26:55 -0500

    Indent

--- loggingtest.py
+++ loggingtest.py
@@ -16,7 +16,7 @@ class StackdriverJsonFormatter(jsonlogger.JsonFormatter, object):
 
     def process_log_record(self, log_record):
         log_record['severity'] = log_record['levelname']
-            del log_record['levelname']
+        del log_record['levelname']
         return super(StackdriverJsonFormatter, self).process_log_record(log_record)
 
 logging.debug("This is a debug log.")
