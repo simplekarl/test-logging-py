997fab3cd086932c907e9ae03dc2421d9a5edb1c
Karl Mohler <karl@SimpleLTC.local>
2018-06-28 16:43:11 -0500

    Update

--- loggingtest.py
+++ loggingtest.py
@@ -1,30 +1,44 @@
 import logging
 import sys
 from pythonjsonlogger import jsonlogger
+#import childlogtest
 
-handler = logging.StreamHandler(sys.stdout)
-formatter = StackDriverJsonFormatter()
-handler.setFormatter(formatter)
+class StackdriverJSONFormatter(jsonlogger.JsonFormatter):
+    def add_fields(self, log_record, record, message_dict):
+        super(StackdriverJSONFormatter, self).add_fields(log_record, record, message_dict)
+        if not log_record.get('time'):
+            # this doesn't use record.created, so it is slightly off
+            #now = datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            #log_record['timestamp'] = now
+            log_record['time'] = record.created
+        if log_record.get('level'):
+            log_record['level'] = log_record['level'].upper()
+        else:
+            log_record['level'] = record.levelname
 
-root_logger = logging.getLogger()
-root_logger.addHandler(handler)
+def setup_stackdriver_logging(log_level=logging.INFO):
+    formatter = StackdriverJSONFormatter('(time) (level) (name) (message)')
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formatter)
+    root_logger = logging.getLogger()
+    root_logger.addHandler(handler)
+    root_logger.setLevel(log_level)
 
-class StackdriverJsonFormatter(jsonlogger.JsonFormatter, object):
+class LoggingTest():
 
-    def __init__(self, fmt="%(levelname) %(message)", style='%', *args, **kwargs):
-        jsonlogger.JsonFormatter.__init__(self, fmt=fmt, *args, **kwargs)
+    def __init__(self):
+        logger = logging.getLogger(__name__)
+        logger.debug("This is a debug log.")
+        logger.info("This is an info log.")
+        logger.warn("This is a warn log.")
+        logger.error("This is an error log.")
+        logger.critical("This is a critical log.")
+        logger.exception("This is an exception log.")
+        logger.warning("This is a warning log.")
 
-    def process_log_record(self, log_record):
-        log_record['severity'] = log_record['levelname']
-        del log_record['levelname']
-        return super(StackdriverJsonFormatter, self).process_log_record(log_record)
+  #  def subtest():
+  #      childlogtest.run()
 
-logging.debug("This is a debug log.")
-logging.info("This is an info log.")
-logging.warn("This is a warn log.")
-logging.error("This is an error log.")
-logging.critical("This is a critical log.")
-logging.exception("This is an exception log.")
-logging.warning("This is a warning log.")
-
-logging.error('{"severity":"WARNING","message":"Testing warning"}')
+if __name__ == '__main__':
+    setup_stackdriver_logging()
+    test = LoggingTest()
